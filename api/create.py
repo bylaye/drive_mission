80e0eef798c463389932c9e33e5e4bbd8e3ffe7a
bylaye <abdoulayeniang2010@gmail.com>
2024-05-16 21:30:10 +0000

    create Mission

--- api/create.py
+++ api/create.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from schemas.Engin import Engin, EnginCreate
 from schemas.Chauffeur import Chauffeur, ChauffeurCreate
+from schemas.Mission import Mission, MissionCreate
 from typing import List
 import models
 
@@ -18,3 +19,10 @@ def add_chauffeur(db: Session, chauffeur=ChauffeurCreate):
     db.add(db_chauffeur)
     db.commit()
     return db_chauffeur
+
+
+def add_mission(db:Session, mission=MissionCreate):
+    db_mission = models.MissionModel(**mission.model_dump())
+    db.add(db_mission)
+    db.commit()
+    return db_mission
